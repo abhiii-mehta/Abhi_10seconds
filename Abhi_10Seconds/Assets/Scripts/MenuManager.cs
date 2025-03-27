027465fde4d785f974ec3ac7deb6514dad895006
Abhinandan <abhimehta1324@gmail.com>
2025-03-27 19:40:25 +0100

    sound menu

--- Abhi_10Seconds/Assets/Scripts/MenuManager.cs
+++ Abhi_10Seconds/Assets/Scripts/MenuManager.cs
@@ -3,6 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject soundPanel;
+    private bool soundPanelVisible = false;
+    public void ToggleSoundPanel()
+    {
+        soundPanelVisible = !soundPanelVisible;
+        soundPanel.SetActive(soundPanelVisible);
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene");
@@ -17,4 +24,19 @@ public class MenuManager : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void CloseSoundPanel()
+    {
+        soundPanelVisible = false;
+        soundPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (soundPanelVisible && Input.GetKeyDown(KeyCode.Escape))
+        {
+            CloseSoundPanel();
+        }
+    }
+
 }
